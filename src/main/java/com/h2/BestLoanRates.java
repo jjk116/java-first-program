d14e3472574126e5bfe218b9fbc4401716e1b05e
James Kings <31575715+jjk116@users.noreply.github.com>
2023-02-04 21:46:09 +0000

    Completed Module

---
+++ src/main/java/com/h2/BestLoanRates.java
@@ -0,0 +1,13 @@
+package com.h2;
+
+import java.util.Scanner;
+
+public class BestLoanRates {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter you name");
+        String name = scanner.nextLine();
+
+        System.out.println("Hello " +  name);
+    };
+}

8aa994ee99caab84479cc9c904b531af545d9841
James Kings <31575715+jjk116@users.noreply.github.com>
2023-02-07 18:34:20 +0000

    Completed Module

---
+++ src/main/java/com/h2/MortgageCalculator.java
@@ -0,0 +1,22 @@
+package com.h2;
+
+public class MortgageCalculator {
+    private long loanAmount;
+    private int termInYears;
+    private float annualRate;
+    private double monthlyPayment;
+
+    public MortgageCalculator(long loanAmount, int termInYears, float annualRate) {
+        this.loanAmount = loanAmount;
+        this.termInYears = termInYears;
+        this.annualRate = annualRate;
+    }
+
+    private int getNumberOfPayments() {
+        return termInYears*12;
+    }
+    private float getMonthlyInterestRate() {
+        float interestRate = (annualRate/100)/12;
+        return interestRate;
+    }
+}
